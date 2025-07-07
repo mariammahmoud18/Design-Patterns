dfbb8111ba515dadde518d23e99d63310c71a15c
Mariam Swelam <mariammahmoud62001@gmail.com>
2025-07-07 21:37:51 +0300

    functionality: updating main to choose factory creation

--- Design Patterns/Factory Method/FileGenerator/src/Main.java
+++ Design Patterns/Factory Method/FileGenerator/src/Main.java
@@ -1,9 +1,34 @@
 import factory.CSVReportFactory;
+import factory.PDFReportFactory;
+import factory.excelReportFactory;
 import factory.reportsFactory;
+import factory.wordReportFactory;
+import reports.report;
 
 public class Main {
     public static void main(String[] args) {
-        reportsFactory reportsFactory = new CSVReportFactory();
-        reportsFactory.createReport().generateReport();
+        createFactory("PDF");
+    }
+
+    public static void createFactory(String fileType){
+        reportsFactory reportsFactory = null;
+        switch (fileType) {
+            case "CSV":
+                reportsFactory = new CSVReportFactory();
+                break;
+            case "PDF":
+                reportsFactory = new PDFReportFactory();
+                break;
+            case "excel":
+                reportsFactory = new excelReportFactory();
+                break;
+            case "word":
+                reportsFactory = new wordReportFactory();
+                break;
+            default:
+                break;
+        }
+        report report = reportsFactory.createReport();
+        report.generateReport();
     }
 }
